80c200c3932b237f0690f2b3089a8b14ed5938ee
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-28 20:13:49 -0300

    feat: :sparkles: adding properties to the component

--- 01 - Fundamentos/src/App.jsx
+++ 01 - Fundamentos/src/App.jsx
@@ -3,10 +3,14 @@ import { Post } from "./Post"
 export function App() {
   return (
     <div>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
+      <Post author="Maciel Ferreira" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi minima beatae adipisci! Dignissimos tempore 
+                     magnam voluptate quisquam assumenda nisi eos, ipsam aliquid deleniti consequatur delectus eius, ad eaque recusandae.">
+      </Post>
+      <Post author="DIego Fernandes" 
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, sapiente numquam culpa officiis laboriosam voluptatibus. 
+                     Rerum perferendis cumque dolores numquam suscipit reprehenderit veniam sit, debitis nostrum consequatur maiores aliquid quasi?">
+      </Post>
     </div>
   )
 }
--- 01 - Fundamentos/src/Post.jsx
+++ 01 - Fundamentos/src/Post.jsx
@@ -1,3 +1,16 @@
-export function Post() {
-  return <p>Post</p>
+import PropTypes from 'prop-types';
+
+Post.propTypes = {
+  author: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired
 }
+
+export function Post(props) {
+  return (
+    <div>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </div>
+  )
+}
+
