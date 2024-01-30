bacca92e2127d78daebd8568b552ee75fea041d0
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-29 21:27:51 -0300

    style: :lipstick: add global styles

--- 01 - Fundamentos/src/App.jsx
+++ 01 - Fundamentos/src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post"
+import { Header } from "./components/header/Header"
+import "./global.css"
 
 export function App() {
   return (
     <div>
+      <Header></Header>
       <Post author="Maciel Ferreira" 
             content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi minima beatae adipisci! Dignissimos tempore 
                      magnam voluptate quisquam assumenda nisi eos, ipsam aliquid deleniti consequatur delectus eius, ad eaque recusandae.">
