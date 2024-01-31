1cc5841fb3c53e5090fbee094d354ca172b988a7
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-31 20:43:59 -0300

    style: :lipstick: add rocketseat eslint

--- 01 - Fundamentos/.eslintrc.cjs
+++ 01 - Fundamentos/.eslintrc.cjs
@@ -1,11 +1,8 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+  "extends": [
+    "@rocketseat/eslint-config/react"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
