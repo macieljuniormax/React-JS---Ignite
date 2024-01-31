60350ab2266a11a93d94b18196e92b42205d082d
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-31 20:37:58 -0300

    style: :lipstick: create container to aside, feed and apply styles

--- 01 - Fundamentos/src/App.jsx
+++ 01 - Fundamentos/src/App.jsx
@@ -1,19 +1,29 @@
 import { Post } from "./Post"
 import { Header } from "./components/header/Header"
 import "./global.css"
+import styles from "./App.module.css"
 
 export function App() {
   return (
     <div>
       <Header></Header>
-      <Post author="Maciel Ferreira" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi minima beatae adipisci! Dignissimos tempore 
-                     magnam voluptate quisquam assumenda nisi eos, ipsam aliquid deleniti consequatur delectus eius, ad eaque recusandae.">
-      </Post>
-      <Post author="DIego Fernandes" 
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, sapiente numquam culpa officiis laboriosam voluptatibus. 
-                     Rerum perferendis cumque dolores numquam suscipit reprehenderit veniam sit, debitis nostrum consequatur maiores aliquid quasi?">
-      </Post>
+
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post author="Maciel Ferreira" 
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi minima beatae adipisci! Dignissimos tempore 
+                      magnam voluptate quisquam assumenda nisi eos, ipsam aliquid deleniti consequatur delectus eius, ad eaque recusandae.">
+          </Post>
+          <Post author="DIego Fernandes" 
+                content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, sapiente numquam culpa officiis laboriosam voluptatibus. 
+                        Rerum perferendis cumque dolores numquam suscipit reprehenderit veniam sit, debitis nostrum consequatur maiores aliquid quasi?">
+          </Post>
+        </main>
+      </div>
+      
     </div>
   )
 }
