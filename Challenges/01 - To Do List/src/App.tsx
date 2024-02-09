b61b0b1c115bbcede6633163a5b65b69b244fb54
Maciel Júnior <maciel_custodio@icloud.com>
2024-02-08 22:37:00 -0300

    refactor: :recycle: create wrapper

--- Challenges/01 - To Do List/src/App.tsx
+++ Challenges/01 - To Do List/src/App.tsx
@@ -1,11 +1,14 @@
-import './App.module.css'
 import { Header } from './components/Header'
+import styles from './App.module.css'
 
 function App() {
 
   return (
     <div>
       <Header />
+      <main className={styles.wraper}>
+
+      </main>
     </div>
   )
 }
