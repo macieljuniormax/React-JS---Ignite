c8424fe3c72b7912a6a9358b024ce1c7dd3b280a
Maciel Júnior <maciel_custodio@icloud.com>
2024-02-10 17:46:17 -0300

    feat: :sparkles: add create task button

--- Challenges/01 - To Do List/src/App.tsx
+++ Challenges/01 - To Do List/src/App.tsx
@@ -1,15 +1,16 @@
 import { Header } from './components/Header'
 import { Input } from './components/Input'
 import styles from './App.module.css'
+import { CreateButton } from './components/CreateButton'
 
 function App() {
-
   return (
     <div>
       <Header />
-      <main className={styles.wraper}>
+      <main className={styles.wrapper}>
         <section className={styles.taskCreator}>
           <Input />
+          <CreateButton />
         </section>
       </main>
     </div>
--- Challenges/01 - To Do List/src/components/Input.tsx
+++ Challenges/01 - To Do List/src/components/Input.tsx
@@ -2,6 +2,10 @@ import styles from './Input.module.css'
 
 export function Input() {
   return (
-      <input className={styles.input} type="text" placeholder='Adicione uma nova tarefa'/>
+    <input
+      className={styles.input}
+      type="text"
+      placeholder="Adicione uma nova tarefa"
+    />
   )
 }
